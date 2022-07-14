88f611d6d742e8f2b5d51e14e8837c0db7ec4d12
Lucinotion <51853805+Lucinotion@users.noreply.github.com>
2022-07-14 11:57:08 +0200

    deleted script files from base directory

--- DebugDrawSphere.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DebugDrawSphere : MonoBehaviour
-{
-    void OnDrawGizmos(){
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position, 0.5f);
-    }
-}
